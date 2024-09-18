278134ae779bae6474d94a819079a3ee94f00a41
Nainvi-singh <nainvisingh#@gmail.com>
2024-09-18 18:41:16 +0530

    updated

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -2,12 +2,11 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req,res,next) => {
     try {
-        const { authorization } = req.headers;
+        const token = req.cookies.token;
        
         if(!token){
             return res.status(401).json({message:"User not authenticated"});
         }
-        const token = authorization.replace('Bearer ', '');
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(!decode) {
             return res.status(401).json({message:"Invalid token"});
